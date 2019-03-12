8842398f3ebc0603120be8905710c73aa6afb7c7
Nithichai <rockon23146@gmail.com>
2019-03-12 07:43:10 +0700

    Update

--- intelij.webpack.js
+++ intelij.webpack.js
@@ -8,9 +8,5 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  },
-  devServer: {
-    compress: true,
-    public: 'basic-health-care-device.herokuapp.com'
   }
 };
