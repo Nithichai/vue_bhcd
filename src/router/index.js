f31742de42d2a93de1185b0bc24dae940a75b5c0
Nithichai <rockon23146@gmail.com>
2019-04-28 01:52:45 +0700

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -17,25 +17,25 @@ const router = new VueRouter({
   }
 });
 
-// router.beforeEach((to, from, next) => {
-//   console.log(process.env.CLIENTID)
-//   if (to.path.localeCompare("/login/return") != 0) {
-//     var accessToken = localStorage.access_token
-//     axios({
-//       method: 'get',
-//       url: 'https://api.line.me/v2/profile',
-//       headers: {
-//         'Authorization' : 'Bearer ' + accessToken
-//       },
-//     }).then((response) => {
-//       next()
-//     }).catch((error) => {
-//       localStorage.clear();
-//       window.location.replace("https://access.line.me/oauth2/v2.1/authorize?bot_prompt=normal&response_type=code&redirect_uri=https%3A%2F%2Fbasic-health-care-device.herokuapp.com%2Flogin%2Freturn&scope=profile%20openid%20email&state=KGn1K1mqzr&client_id=1556734128")
-//     })
-//   } else {
-//     next()
-//   }
-// });
+router.beforeEach((to, from, next) => {
+  console.log(process.env.CLIENTID)
+  if (to.path.localeCompare("/login/return") != 0) {
+    var accessToken = localStorage.access_token
+    axios({
+      method: 'get',
+      url: 'https://api.line.me/v2/profile',
+      headers: {
+        'Authorization' : 'Bearer ' + accessToken
+      },
+    }).then((response) => {
+      next()
+    }).catch((error) => {
+      localStorage.clear();
+      window.location.replace("https://access.line.me/oauth2/v2.1/authorize?bot_prompt=normal&response_type=code&redirect_uri=https%3A%2F%2Fbasic-health-care-device.herokuapp.com%2Flogin%2Freturn&scope=profile%20openid%20email&state=KGn1K1mqzr&client_id=1556734128")
+    })
+  } else {
+    next()
+  }
+});
 
 export default router;
